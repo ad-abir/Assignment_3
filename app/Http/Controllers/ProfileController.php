d3028ffc174094c7a397b27baac4e928d0ec8afb
ad-abir <abhishek.das0023@gmail.com>
2024-06-24 23:51:00 +0600

    Final Check

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,29 +9,25 @@ class ProfileController extends Controller
 {
     public function index($id)
     {
-        // Declare variables
         $name = "Donald Trump";
         $age = "75";
 
-        // Create associative array
         $data = [
             'id' => $id,
             'name' => $name,
             'age' => $age
         ];
 
-        // Create cookie
         $cookie = cookie(
-            'access_token', // name
-            '123-XYZ', // value
-            1, // minutes
-            '/', // path
-            $_SERVER['SERVER_NAME'], // domain
-            false, // secure
-            true // httpOnly
+            'access_token',
+            '123-XYZ',
+            1,
+            '/',
+            $_SERVER['SERVER_NAME'],
+            false,
+            true
         );
 
-        // Return response with data, status code and set cookie
         return response()->json($data, 200)->cookie($cookie);
     }
 }
